e90a877a5e6e3faa14128516a73b428caa5ce2d4
Kamirus <listopadkamil@gmail.com>
2017-12-16 16:52:37 +0100

    added const colors

--- lib/color.ml
+++ lib/color.ml
@@ -2,3 +2,9 @@ type t = int * int * int
 
 let create r g b =
   (r, g, b)
+
+let white = create 255 255 255
+let black = create 0 0 0
+let red = create 255 0 0
+let green = create 0 255 0
+let blue = create 0 0 255
--- lib/color.mli
+++ lib/color.mli
@@ -1,3 +1,9 @@
 type t
 
+val white : t
+val black : t
+val red : t
+val green : t
+val blue : t
+
 val create : int -> int -> int -> t
