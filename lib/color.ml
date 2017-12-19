f50d1c186ec00180662664d145fb4e598840eeb5
Kamirus <listopadkamil@gmail.com>
2017-12-19 18:22:38 +0100

    refatored internal color values to floats

--- lib/color.ml
+++ lib/color.ml
@@ -1,26 +1,31 @@
-type t = int * int * int
+type t = float * float * float
 
-let min_val = 0
-let max_val = 255
+let min_val = 0.
+let max_val = 1.
+
+let from_rgb rgb_val = 
+  float_of_int rgb_val /. 255.
+
+let to_rgb x =
+  x *. 255. |> int_of_float
 
 let create r g b =
-  (r, g, b)
+  (from_rgb r, from_rgb g, from_rgb b)
 
 let values (r, g, b) =
-  (r, g, b)
+  (to_rgb r, to_rgb g, to_rgb b)
 
 let add (r1, g1, b1) (r2, g2, b2) = 
-  (r1 + r2, g1 + g2, b1 + b2)
+  (r1 +. r2, g1 +. g2, b1 +. b2)
 
 let mul (r1, g1, b1) (r2, g2, b2) = 
-  (r1 * r2, g1 * g2, b1 * b2)
+  (r1 *. r2, g1 *. g2, b1 *. b2)
 
 let mulf (r, g, b) x = 
-  let f c = float_of_int c *. x |> int_of_float in
-  (f r, f g, f b)
+  (r *. x, g *. x, b *. x)
 
-let fit ?(min_v=min_val) ?(max_v=max_val) (r, g, b) = 
-  let f c = max min_v (min max_v c) in
+let fit (r, g, b) = 
+  let f c = max min_val (min max_val c) in
   (f r, f g, f b)
   
 let white = create 255 255 255
--- lib/color.mli
+++ lib/color.mli
@@ -1,8 +1,5 @@
 type t
 
-val min_val : int
-val max_val : int
-
 val white : t
 val black : t
 val red : t
@@ -14,4 +11,4 @@ val values : t -> int * int * int
 val add : t -> t -> t
 val mul : t -> t -> t
 val mulf : t -> float -> t
-val fit : ?min_v:int -> ?max_v:int -> t -> t
+val fit : t -> t
