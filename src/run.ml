1c30a6310cd04d2de965e5aa3d22e2b8e328aa75
Kamirus <listopadkamil@gmail.com>
2017-12-24 19:15:33 +0100

    toimage from run

--- src/run.ml
+++ src/run.ml
@@ -8,11 +8,15 @@ let render_on_screen filename =
   let resolution = x ^ "x" ^ y in
   Draw.main ~resolution ~raytracer ~cfg:path
 
-(* let filepaths =
-  List.tl @@ Array.to_list Sys.argv
-  |> List.map (fun n -> "cfgs/" ^ n ^ ".json") *)
+let toimage filename = 
+  let path = to_filepath filename in
+  let raytracer, x, y = Parse_cfg.parse path in
+  let x_max = int_of_string x in
+  let y_max = int_of_string y in
+  Toimage.main ~x_max ~y_max ~name:filename ~raytracer
 
 let () =
   match Sys.argv with
   | [| _; filename |] -> render_on_screen filename
+  | [| _; filename; "save" |] -> toimage filename
   | _ -> failwith "provide config file name"
--- src/toimage.ml
+++ src/toimage.ml
@@ -1,11 +1,12 @@
-let () =
-  let width  = 800
-  and length = 600
-  and name   = "cool-pic.png" in
-  let image = Image.create_rgb 800 600 in
-  for x = 0 to 49 do
-    for y = 0 to 100 do
-      Image.write_rgb image x y 255 0 0;
-    done;
+let main ~x_max ~y_max ~name ~raytracer =
+  let name = "pics/" ^ name ^ ".png" in
+  let image = Image.create_rgb x_max y_max in
+  
+  for x = 0 to x_max - 1 do
+    for y = 0 to y_max - 1 do
+      let c = raytracer x (y_max - 1 - y) in
+      let r, g, b = Color.values c in
+      Image.write_rgb image x y r g b
+    done
   done;
   ImageLib.PNG.write_png name image
